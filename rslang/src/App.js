282de3384c46a9acdf7f0911016fad591f7824ad
Alexey <Shaht3r@gmail.com>
2020-06-13 02:27:16 +0300

    init: add react, redux, bootstrap, eslint

---
+++ rslang/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          Hello Word
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
