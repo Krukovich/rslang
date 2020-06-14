0c85c26c3b4f2f134875f836a93ae1e6350367bf
alexger95 <alexger95@gmail.com>
2020-06-14 14:10:44 +0300

    feat: create login and createaccaunt Components

--- rslang/src/App.js
+++ rslang/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import PlayZonePage from './Components/PlayZonePage/PlayZonePage';
 import Route from './Router/Routes';
+import Login from './Components/Login/Login'
+import CreateAccount from './Components/CreateAccount/CreateAccount'
+
 
 const App = () => {
   
@@ -11,6 +14,8 @@ const App = () => {
         <div className="col-lg-12">
           <PlayZonePage />
         </div>
+        <Login />
+        <CreateAccount />
       </div>
       <Switch>
         {Route}
