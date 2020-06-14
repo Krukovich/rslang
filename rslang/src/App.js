fc61f2153ebb5382ee0bdb7668a9278e5fac05f6
alexger95 <alexger95@gmail.com>
2020-06-14 21:18:31 +0300

    feat: create router folder, add router array

--- rslang/src/App.js
+++ rslang/src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Switch } from 'react-router-dom';
+import Route from './Router/Routes'
 
 const App = () => {
+  
   return (
     <div className="container">
       <div className="row">
-        <div className="col-lg-12">
-          Hello Word
-        </div>
+        <div className="col-lg-12">Hello Word</div>
       </div>
       <Switch>
-          <Route path="/" exact render={() => <h1>Home Page</h1>}/>
-          {/* <Route path="/login" exact component={Login}/> */}
-          {/* <Route path="/about" component={About}/> */}
-          {/* <Route path="/minigames/:name" component={MiniGames}/> */}
-          <Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>}/>
-        </Switch>
+        {Route}
+      </Switch>
     </div>
   );
 };
