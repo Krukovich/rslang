dfca0c870e232e6aca181f4df5a80271ae7a4459
Alexey <Shaht3r@gmail.com>
2020-06-16 13:05:26 +0300

    feat: add play card

--- rslang/src/App.js
+++ rslang/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import Route from './Router/Routes'
+import PlayZone from './Component/PlayZone/PlayZone';
+import Route from './Router/Routes';
 
 const App = () => {
   
   return (
     <div className="container">
       <div className="row">
-        <div className="col-lg-12">Hello Word</div>
+        <div className="col-lg-12">
+          <PlayZone />
+        </div>
       </div>
       <Switch>
         {Route}
