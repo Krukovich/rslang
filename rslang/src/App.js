ab7f09d292ccd430981bc4731f2de1e1e3a71f3c
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-06-16 21:11:49 +0300

    init: start main-page & add simple layout

--- rslang/src/App.js
+++ rslang/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Router/Routes'
+import MainPage from './MainPage/MainPage'
 
 const App = () => {
-  
+
   return (
     <div className="container">
       <div className="row">
-        <div className="col-lg-12">Hello Word</div>
+        <div className="col-md-12">
+          <MainPage />
+        </div>
       </div>
       <Switch>
         {Route}
---
+++ rslang/src/MainPage/Card/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <a className="card border-primary rounded">
+            <div className="row no-gutters">
+                <div className="col-md-4 d-flex justify-content-center align-items-center bg-primary">
+                    <div className="card-img fluid h-75 w-75" style={{ backgroundColor: 'white' }}></div>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body bg-primary" style={{ color: 'white' }}>
+                        <h5 className="card-title">{props.title}</h5>
+                        <p className="card-text">{props.description}</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default Card
