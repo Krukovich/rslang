30ab5284d148bc12756bdfd6aad9c31f553b31c7
alexger95 <alexger95@gmail.com>
2020-06-19 16:00:17 +0300

    fix: REGEXP Password

--- rslang/src/constant.js
+++ rslang/src/constant.js
@@ -59,6 +59,9 @@ export const wordCards = {
   ]
 };
 
+
+
 export const API_LINK = 'https://afternoon-falls-25894.herokuapp.com/';
-export const PASSWORD_PATTERN = '(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$';
+const PASSWORD_PATTERN_REGEXP =  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&_+-.,[{:;\}\]])[A-Za-z\d@$!%*#?&_+-.,[{:;\}\]]{8,}$/;
+export const PASSWORD_PATTERN = PASSWORD_PATTERN_REGEXP.toString().slice(1).slice(0, -1);
 export const PASSWORD_REQUIREMENTS = "Пароль должен содержать одну цифру, один спецсимвол из +-_@$!%*?&#.,;:[]{} и одну заглавную букву";
