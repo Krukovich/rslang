b2c74b33a9121141471c23c26e9226279642d1a8
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-06-19 20:35:57 +0300

    fix: merge conflict

--- rslang/src/App.js
+++ rslang/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import { RouteMap } from './Components/Router/Routes';
@@ -19,26 +18,3 @@ const App = () => {
 };
 
 export default App;
-=======
-import React from 'react';
-import { Switch } from 'react-router-dom';
-import { RouteMap } from './Components/Router/Routes';
-
-const App = () => {
-
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-        <Switch>
-          {RouteMap}
-        </Switch>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default App;
-
->>>>>>> a89fc155d40a754e8553fe66ef2daf77d6d9d3df
