faeb56bf5f1194d937dbd4001a2430ee917f3a41
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-06-19 20:24:16 +0300

    fix: trying to merge branches

--- rslang/src/App.js
+++ rslang/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import Route from './Router/Routes'
-import MainPage from './Pages/MainPage/MainPage'
+import Route from './Router/Routes';
 
 const App = () => {
 
   return (
     <div className="container-fluid">
-      <MainPage />
-
       <Switch>
         {Route}
       </Switch>
