9e207aabdb8697b71c616f502d0e7c0a25cff5d1
Alexey <Shaht3r@gmail.com>
2020-06-22 01:35:52 +0300

    fix: fix merge bug

--- rslang/src/Pages/LongStats/LongStats.js
+++ rslang/src/Pages/LongStats/LongStats.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Line} from 'react-chartjs-2';
-import './LongStats.scss';
+import './longStats.scss';
 
 const state = {
   labels: [],
