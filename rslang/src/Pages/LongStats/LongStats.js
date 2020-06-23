8bec6399addbeee8e0f3f346e45d0532da72ab85
Alexey <Shaht3r@gmail.com>
2020-06-24 00:54:48 +0300

    fix: fix header position

--- rslang/src/Pages/LongStats/LongStats.js
+++ rslang/src/Pages/LongStats/LongStats.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Line} from 'react-chartjs-2';
+
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import Header from '../../Components/Header/Header';
+
 import './longStats.scss';
 
 const state = {
@@ -12,7 +16,8 @@ const state = {
       borderWidth: 2,
       data: []
     }
-  ]
+  ],
+  clicked: false,
 }
 
 export default class LongStats extends React.Component {
@@ -24,24 +29,32 @@ export default class LongStats extends React.Component {
     console.log(this.totalWords, props.totalNewWords);
   }
 
+  asideToggle() {
+    state.clicked = !state.clicked;
+  }
+
   render() {      
     return (
-      <div className="graph">
-        <Line
-          data={state}
-          options={{
-            title:{
-              display:true,
-              text:'Всего слов',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
-      </div>
+      <React.Fragment>
+        <Sidebar clicked={ state.clicked }/>
+        <Header asideToggle={ this.asideToggle } clicked={ state.clicked }/>
+        <div className="graph pt-5">
+          <Line
+            data={state}
+            options={{
+              title:{
+                display:true,
+                text:'Всего слов',
+                fontSize:20
+              },
+              legend:{
+                display:true,
+                position:'right'
+              }
+            }}
+          />
+        </div>
+      </React.Fragment>
     );
   }
  }
