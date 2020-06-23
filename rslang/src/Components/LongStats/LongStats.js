2ffc2a1a5abdbf6df27d1191ff908c128f59455d
alexger95 <alexger95@gmail.com>
2020-06-23 21:41:04 +0300

    resolve merge

--- rslang/src/Components/LongStats/LongStats.js
+++ rslang/src/Components/LongStats/LongStats.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Line} from 'react-chartjs-2';
-//import './longStats.scss';
+import './longStats.scss';
 
 const state = {
   labels: ['1', '2', '3',
