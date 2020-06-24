cb57c16c8f084d1174086b07025533e87a4783d9
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-06-24 17:01:31 +0300

    feat: stats from server BETA

---
+++ rslang/src/Components/ShortStats/ShortStats.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './shortStats.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDragon } from '@fortawesome/free-solid-svg-icons';
+import { faCrown } from '@fortawesome/free-solid-svg-icons';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { faUserGraduate } from '@fortawesome/free-solid-svg-icons';
+import english from './english.gif';
+
+const ShortStatsHeader = () => {
+    return (
+        <div className="row align-items-center flex-column shortStatsHeader">
+            <img src={english} alt="stats pic" />
+            <h1>Серия завершена</h1>
+        </div>
+    )
+}
+
+const ShortStatsInfo = (props) => {
+    const { total, right, newWords, rightInARow } = props; 
+    return (
+        <ul className="row align-items-center flex-column shortStatsInfo">
+            <li className="shortStatsInfo-item d-flex"><span><FontAwesomeIcon icon={ faDragon } /> Карточек завершено:  </span><span>{total}</span></li>
+            <li className="shortStatsInfo-item d-flex"><span><FontAwesomeIcon icon={ faCrown } /> Правильные ответы: </span> <span>{right}%</span></li>
+            <li className="shortStatsInfo-item d-flex"><span><FontAwesomeIcon icon={ faCartPlus } /> Новые слова: </span> <span>{newWords}</span> </li>
+            <li className="shortStatsInfo-item d-flex"><span><FontAwesomeIcon icon={ faUserGraduate } /> Правильных ответов подряд: </span> <span>{rightInARow}</span> </li>
+        </ul>
+    )
+}
+export default class ShortStats extends React.Component {
+    render() {
+        return (
+            <div className="container shortStats">
+                <ShortStatsHeader />
+                <ShortStatsInfo {...this.props}  />
+            </div>
+        )
+    }
+};
+
---
+++ rslang/src/Pages/LongStats/LongStats.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import {Line} from 'react-chartjs-2';
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import './longStats.scss';
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZjExOTlhYWEyNDVlMDAxN2E1NzhmMCIsImlhdCI6MTU5MzAwNDY3NiwiZXhwIjoxNTkzMDE5MDc2fQ.4bChZYLqFi411oUYRTQqyMEfBfb3g962YysjdPqSfkc";
+const userId = "5ef1199aaa245e0017a578f0";
+
+const getStats = async () => {
+  const rawResponse = await fetch(`https://afternoon-falls-25894.herokuapp.com/users/${userId}/statistics`, {
+    method: 'GET',
+    withCredentials: true,
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  });
+  const content = await rawResponse.json();
+  const stats = JSON.parse(content.optional.dailyNewWords);
+
+  console.log(stats, typeof stats, Array.isArray(stats));
+  return stats;
+};
+
+const ProgressLabel = () => {
+  return (
+    <div className="longStatsElem-label d-flex justify-content-center">Изучено слов из словаря</div>
+  )
+}
+
+// getStats().then(result => {
+//   console.log(result)});
+
+export default class LongStats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.wordsNow = Math.ceil((props.totalNewWords[props.totalNewWords.length - 1] * 100) / this.props.totalWords);
+    this.dataLabels = props.dataLabels;
+    this.state = {
+      labels: [...props.dataLabels],
+      datasets: [
+        {
+          label: 'Прогресс',
+          borderColor: 'rgba(0,0,0,1)',
+          backgroundColor: 'orange',
+          borderWidth: 2,
+          data: [] //...props.totalNewWords
+        },
+        {
+          label: 'Слов изучено в день',
+          borderColor: 'tomato',
+          backgroundColor: 'tomato',
+          data: [], //...props.dailyNew
+          fill: false,
+        }
+      ]
+    };
+  }
+
+  getSum(arr) {
+    let prev = 0;
+    return arr.map((elem) => {
+      prev += elem;
+      return prev;
+    })
+  }
+
+  componentDidMount() {   
+    this._asyncRequest = getStats().then(
+      result => {
+        this.state.datasets[0].data = this.getSum(result);
+        this.state.datasets[1].data = result;
+        this._asyncRequest = null;
+        this.setState({result});
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    if(this._asyncRequest) {
+      this._asyncRequest.cancel();
+    }
+  }
+  
+  render() {      
+    return ( 
+      <div>
+        <div className="graph longStatsElem">
+          <Line
+            data={this.state}
+            // getElementAtEvent={dataset => console.log(dataset, dataset[0]._index)} // shows the dataset elements
+            options={{
+              title: {
+                display: true,
+                text: 'Все слова',
+                fontSize: 20
+              },
+              legend: {
+                display: true,
+                position: 'bottom'
+              },
+              tooltips: {
+                mode: 'index',
+                intersect: true
+              }
+            }}
+          />
+        </div>
+        <div className="longStatsElem row d-flex justify-content-center">
+          <div className="col-md-8 ">
+            <ProgressBar variant="success" animated min={0} now={this.wordsNow} label={`${this.wordsNow}%`} />
+            <ProgressLabel />
+          </div>
+        </div>
+      </div>  
+    );
+  }
+ }
+
+
