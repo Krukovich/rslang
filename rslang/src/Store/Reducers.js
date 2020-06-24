0e4059bd5ccdc19405624e59706680c130f1f546
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-06-24 17:01:31 +0300

    feat: stats from server BETA

--- rslang/src/Store/Reducers.js
+++ rslang/src/Store/Reducers.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
->>>>>>> fix
 import { combineReducers } from "redux";
 import { playZonePageReducer } from "./PlayZonePage/reducers";
 import { sprintGameReducer } from "./SprintGame/Reducers";
 import { fortuneGameReducer } from "./FortuneGame/Reducers";
 import { savannaReducer } from "./Savanna/reducers";
 import * as Actions from "./Actions";
-=======
-import { combineReducers } from 'redux';
-import { playZonePageReducer } from './PlayZonePage/reducers';
-import { sprintGameReducer } from './SprintGame/Reducers';
-import { statsReducer } from './Longs/reducers';
-import * as Actions from './Actions';
->>>>>>> fix
+
 
 const initialState = {
   level: 1, // number indicating difficult level
@@ -120,9 +110,6 @@ export default combineReducers({
   savanna: savannaReducer,
   appSettings: appSettingsReducer,
   sprintGame: sprintGameReducer,
-<<<<<<< HEAD
   fortuneGame: fortuneGameReducer,
-=======
->>>>>>> fix
   miniGamesStats: statsReducer,
 });
