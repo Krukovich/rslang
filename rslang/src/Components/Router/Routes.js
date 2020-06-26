4b81fe0fd12df2102b9a26f92b37c0f16b0e0ac6
alexger95 <alexger95@gmail.com>
2020-06-26 17:52:34 +0300

    feat: router works

--- rslang/src/Components/Router/Routes.js
+++ rslang/src/Components/Router/Routes.js
@@ -18,6 +18,7 @@ import { AudioCall } from '../../Pages/MiniGames/AudioCall/AudioCall.jsx';
 import MainPage from '../../Pages/MainPage/MainPage';
 import Vocabulary from '../../Pages/Vocabulary/vocabulary';
 import { Start } from '../../Pages/Start/Start'
+import { CheckLogin } from '../../Pages/Authentication/CheckLogin'
 
 const sourceOpenRoutes = [
   {
@@ -111,90 +112,82 @@ export function RouteMap() {
     <div className="router">
 
       <Switch>
-      {/* Фантастический код рожались эти строки 3 дня за подробностями прошу в ЛС*/}
-      {sourceOpenRoutes.map(({ path, component }, key) => <Route exact path={path} component={component} key={key} />)}
-      {sourceCloseRoutes.map(({ path, component }, key) => return (<PrivateRoute exact path={path} key={key} >{component}</PrivateRoute>))}
+        {/* Фантастический код рожались эти строки 3 дня за подробностями прошу в ЛС*/}
+        {sourceOpenRoutes.map(({ path, component }, key) => <Route exact path={path} component={component} key={key} />)}
+        {sourceCloseRoutes.map(({ path, component }, key) => <PrivateRoute exact component={component} path={path} key={key} />)}
       </Switch>
     </div>
 
   );
 }
 
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    fakeAuth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    fakeAuth.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
+// const fakeAuth = {
+//   isAuthenticated: false,
+//   authenticate(cb) {
+//     fakeAuth.isAuthenticated = true;
+//     setTimeout(cb, 100); // fake async
+//   },
+//   signout(cb) {
+//     fakeAuth.isAuthenticated = false;
+//     setTimeout(cb, 100);
+//   }
+// };
 
-function AuthButton() {
-  let history = useHistory();
+// function AuthButton() {
+//   let history = useHistory();
 
-  return fakeAuth.isAuthenticated ? (
-    <p>
-      Welcome!{" "}
-      <button
-        onClick={() => {
-          fakeAuth.signout(() => history.push("/"));
-        }}
-      >
-        Sign out
-      </button>
-    </p>
-  ) : (
-      <p>You are not logged in.</p>
-    );
-}
+//   return fakeAuth.isAuthenticated ? (
+//     <p>
+//       Welcome!{" "}
+//       <button
+//         onClick={() => {
+//           fakeAuth.signout(() => history.push("/"));
+//         }}
+//       >
+//         Sign out
+//       </button>
+//     </p>
+//   ) : (
+//       <p>You are not logged in.</p>
+//     );
+// }
 
 // A wrapper for <Route> that redirects to the login
 // screen if you're not yet authenticated.
-function PrivateRoute({ children, ...rest }) {
+function PrivateRoute({ component: Component, ...rest }) {
+  //console.log('private comp', Component)
   return (
     <Route
       {...rest}
-      render={({ location }) =>
-        true ? (
-          children
-        ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: location }
-              }}
-            />
-          )
+      render={(props) =>
+        CheckLogin() ? (
+          <Component {...props} />
+        ) : <Redirect
+            to="/login"
+          />
       }
     />
   );
 }
 
+// function LoginPage() {
+//   let history = useHistory();
+//   let location = useLocation();
 
-function LoginPage() {
-  let history = useHistory();
-  let location = useLocation();
+//   let { from } = location.state || { from: { pathname: "/" } };
+//   let login = () => {
+//     fakeAuth.authenticate(() => {
+//       history.replace(from);
+//     });
+//   };
 
-  let { from } = location.state || { from: { pathname: "/" } };
-  let login = () => {
-    fakeAuth.authenticate(() => {
-      history.replace(from);
-    });
-  };
-
-  return (
-    <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
-    </div>
-  );
-}
-
-
-
+//   return (
+//     <div>
+//       <p>You must log in to view the page at {from.pathname}</p>
+//       <button onClick={login}>Log in</button>
+//     </div>
+//   );
+// }
 
 // const RouteMap = sourceRoutes.map(({ path, component }, key) => (
 //   <Route exact path={path} component={component} key={key} />
--- rslang/src/Pages/Start/Start.jsx
+++ rslang/src/Pages/Start/Start.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 import './Start.scss'
+import { useHistory } from "react-router-dom";
 
 export function Start() {
+    let history = useHistory();
     return (
             <div className="cover-container d-flex h-100 p-3 flex-column align-items-center animatedbackground vh-100">
                 <div className="textstart">
                     <p className="text-center">Хорошая игра - бесплатно и без рекламы</p>
                 </div>
-               <button type="button" class="btnstart btn btn-info">Start game</button>
+               <button type="button" className="btnstart btn btn-info" onClick={() => history.push("/login")}>Start game</button>
             </div>
     )
 }
