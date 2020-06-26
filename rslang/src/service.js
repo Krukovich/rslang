b713b777a25ff9c3d911f9afd86493b7a789c656
Alexey <Shaht3r@gmail.com>
2020-06-26 23:15:29 +0300

    refactor: refactor code add image

--- rslang/src/service.js
+++ rslang/src/service.js
@@ -31,36 +31,21 @@ export const playExampleSound = (src) => {
 }
 
 export const renderPlayString = (data, handlerChange) => {
-  const regexp = /<[^<>]+>/g;
-  const word = data.word;
+  const regexp = /<b>([^<]+)<\/b>/;
+  const word = data.textExample.match(regexp)[1];
   const inputStyle = { width: `${ word.length }5px` };
-  let tempString;
-
-  tempString = data.textExample.replace(regexp, '');
-  tempString = tempString.replace(data.word, '__');
-  const tempCharArray = tempString.split(' ');
-
-  const string = tempCharArray.map((char, index) => {
-    if (char === '__' || char === '__.' ) {
-      return(
-        <span className="m-1" key={ index }>
-          <input
-            className="WordInput"
-            type="text"
-            autoFocus
-            onChange={ handlerChange }
-            maxLength={ word.length }
-            style={ inputStyle }
-          />
-        </span>
-      );
-    } else {
-      return(
-        <span className="m-1" key={ index }>
-          { char }
-        </span>
-      );
-    }
-  });
-  return string;
+  const input =
+    <span className="pl-1">
+      <input
+        className="WordInput"
+        type="text"
+        autoFocus
+        onChange={ handlerChange }
+        maxLength={ word.length }
+        style={ inputStyle }
+      />
+    </span>
+  const parts = data.textExample.split(regexp).map((part) => <span className="pl-1">{ part }</span>)
+  parts.splice(1, 1, input);
+  return parts;
 }
