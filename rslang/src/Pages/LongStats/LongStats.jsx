1464480365562116fef61c192fa7542fb52a12fb
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-02 00:22:20 +0300

    fix, refactor

--- rslang/src/Pages/LongStats/LongStats.jsx
+++ rslang/src/Pages/LongStats/LongStats.jsx
@@ -3,10 +3,7 @@ import { connect } from 'react-redux';
 import { Line } from 'react-chartjs-2';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import './longStats.scss';
-<<<<<<< HEAD
 import BtnsBar from './BtnsBar/BtnsBar';
-=======
->>>>>>> fix: data from server
 
 const miniGameStats = (store) => {
   const { minigameSavannaStats } = store.savanna;
@@ -37,14 +34,7 @@ const getStats = async () => {
     },
   });
   const content = await rawResponse.json();
-<<<<<<< HEAD
   let stats = content.optional.optional;
-=======
-  console.log(content);
-  let stats = content.optional.optional.wordStat;
-
-  console.log(stats);
->>>>>>> fix: data from server
   return stats;
 };
 
@@ -58,16 +48,7 @@ class LongStats extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: false,
-<<<<<<< HEAD:rslang/src/Pages/LongStats/LongStats.jsx
-      wordsNow: 0, // Math.ceil((props.totalNewWords[props.totalNewWords.length - 1] * 100) / this.props.totalWords),
-=======
-<<<<<<< HEAD
       wordsNow: 0,// Math.ceil((props.totalNewWords[props.totalNewWords.length - 1] * 100) / this.props.totalWords),
-=======
-      wordsNow: Math.ceil((props.totalNewWords[props.totalNewWords.length - 1] * 100) / this.props.totalWords),
->>>>>>> fix: data from server
->>>>>>> fix: data from server:rslang/src/Pages/LongStats/LongStats.js
       labels: [], // props.dataLabels,
       datasets: [
         {
@@ -138,7 +119,6 @@ class LongStats extends React.Component {
   componentDidMount() {   
     this._asyncRequest = getStats().then(
       result => {
-<<<<<<< HEAD
         const resultWords = result.wordStat.map((item) => {
           const elem = item.newWords;
           return elem;
@@ -151,20 +131,6 @@ class LongStats extends React.Component {
         this.state.datasets[1].data = resultWords;
         this.state.labels = resultDate;
         this.state.wordsNow = Math.ceil((this.state.datasets[0].data[this.state.datasets[0].data.length-1] * 100) / this.props.totalWords);
-=======
-        const resultWords = result.map((item) => {
-          const elem = item.newWords;
-          return elem;
-        }); 
-        const resultDate = result.map((item) => {
-          const date = new Date(item.timestamp).toString().slice(4, 15);
-          return date;
-        })
-        console.log(resultWords)
-        this.state.datasets[0].data = this.getSum(resultWords);
-        this.state.datasets[1].data = resultWords;
-        this.state.labels = resultDate;
->>>>>>> fix: data from server
         this._asyncRequest = null;
         this.setState({result});
       }
@@ -205,11 +171,11 @@ class LongStats extends React.Component {
           <div className="col-md-8">
             <ProgressBar variant="success" min={0} now={this.state.wordsNow} label={`${this.state.wordsNow}%`} />
             <ProgressLabel />
+           </div> 
             <div className="longStatsElem">
-            <BtnsBar items={items} showStats={this.showStats} />
-            <div className="longStatsElem-field"></div>
+              <BtnsBar items={items} showStats={this.showStats} />
+              <div className="longStatsElem-field"></div>
             </div>         
-          </div>
         </div>
       </React.Fragment>
     );
