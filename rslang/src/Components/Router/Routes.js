a97b271f384cbc4fa71f433c21d5f06c12985415
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-02 00:22:20 +0300

    fix, refactor

--- rslang/src/Components/Router/Routes.js
+++ rslang/src/Components/Router/Routes.js
@@ -87,63 +87,7 @@ const sourceCloseRoutes = [
   },
   {
     path: '/long-stats',
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     component: () => <LongStats totalWords={80} />,
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> feat: stats from server BETA
-=======
->>>>>>> feat: stats from server BETA
-    component: () => <LongStats dataLabels={['день 1', 'день 2', 'день 3',
-      'день 4', 'день 5', 'день 6', 'день 7', 'день 8', 'день 9', 'день 10']}
-      totalNewWords={[3, 5, 7, 9, 10, 16, 17, 20, 22, 28]} totalWords={80}
-      dailyNew={[4, 2, 7, 5, 5, 2, 6, 5, 4, 5]} />,
-<<<<<<< HEAD
-=======
-=======
-
-=======
->>>>>>> fix, refactor
-=======
->>>>>>> fix, refactor
-=======
-=======
-
->>>>>>> feat: stats from server BETA
->>>>>>> feat: stats from server BETA
-=======
->>>>>>> fix: token & id
-    component: () => <LongStats dataLabels={['день 1', 'день 2', 'день 3',
-    'день 4', 'день 5', 'день 6', 'день 7', 'день 8', 'день 9', 'день 10']} 
-    totalNewWords={[3, 5, 7, 9, 10, 16, 17, 20, 22, 28]} totalWords={80} 
-    dailyNew={[4, 2, 7, 5, 5, 2, 6, 5, 4, 5]} />,
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> feat: stats from server BETA
->>>>>>> feat: stats from server BETA
-=======
->>>>>>> fix, refactor
-=======
-    component: () => <LongStats totalWords={80} />,
->>>>>>> fix, refactor
-=======
->>>>>>> fix, refactor
-=======
-
-    component: () => <LongStats dataLabels={['день 1', 'день 2', 'день 3',
-    'день 4', 'день 5', 'день 6', 'день 7', 'день 8', 'день 9', 'день 10']} 
-    totalNewWords={[3, 5, 7, 9, 10, 16, 17, 20, 22, 28]} totalWords={80} 
-    dailyNew={[4, 2, 7, 5, 5, 2, 6, 5, 4, 5]} />,
->>>>>>> feat: stats from server BETA
     exact: true,
   },
   {
