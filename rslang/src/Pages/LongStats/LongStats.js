33247a0c3cac2e7bc1c13a62b24a20a1e81f2659
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-02 00:22:20 +0300

    fix, refactor

---
+++ rslang/src/Pages/LongStats/LongStats.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import './longStats.scss';
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+const token = getCookie("token");
+const userId = getCookie("userId");
+
+const getStats = async () => {
+  const rawResponse = await fetch(`https://afternoon-falls-25894.herokuapp.com/users/${userId}/statistics`, {
+    method: 'GET',
+    withCredentials: true,
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  });
+  const content = await rawResponse.json();
+  let stats = content.optional.optional.wordStat;
+  return stats;
+};
+
+const ProgressLabel = () => {
+  return (
+    <div className="longStatsElem-label d-flex justify-content-center">Изучено слов из словаря</div>
+  )
+}
+
+export default class LongStats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      wordsNow: 0,// Math.ceil((props.totalNewWords[props.totalNewWords.length - 1] * 100) / this.props.totalWords),
+      labels: [], // props.dataLabels,
+      datasets: [
+        {
+          label: 'Прогресс',
+          borderColor: 'rgba(0,0,0,1)',
+          backgroundColor: 'orange',
+          borderWidth: 2,
+          data: [] //...props.totalNewWords
+        },
+        {
+          label: 'Слов изучено в день',
+          borderColor: 'tomato',
+          backgroundColor: 'tomato',
+          data: [], //...props.dailyNew,
+          fill: false,
+        }
+      ]
+    }
+  }
+
+  asideToggle = () => {
+    this.setState({ clicked: !this.state.clicked });
+  }
+
+  getSum(arr) {
+    let prev = 0;
+    return arr.map((elem) => {
+      prev += elem;
+      return prev;
+    })
+  }
+
+  componentDidMount() {   
+    this._asyncRequest = getStats().then(
+      result => {
+        const resultWords = result.map((item) => {
+          const elem = item.newWords;
+          return elem;
+        }); 
+        const resultDate = result.map((item) => {
+          const date = new Date(item.timestamp).toString().slice(4, 15);
+          return date;
+        })
+        this.state.datasets[0].data = this.getSum(resultWords);
+        this.state.datasets[1].data = resultWords;
+        this.state.labels = resultDate;
+        this.state.wordsNow = Math.ceil((this.state.datasets[0].data[this.state.datasets[0].data.length-1] * 100) / this.props.totalWords);
+        this._asyncRequest = null;
+        this.setState({result});
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    if(this._asyncRequest) {
+      this._asyncRequest.cancel();
+    }
+  }
+  
+  render() {      
+    return ( 
+      <React.Fragment>
+      <div className="graph longStatsElem col-md-9">
+          <Line
+            data={this.state}
+            options={{
+              title: {
+                display: true,
+                text: 'Все слова',
+                fontSize: 20
+              },
+              legend: {
+                display: true,
+                position: 'bottom'
+              },
+              tooltips: {
+                mode: 'index',
+                intersect: true
+              }
+            }}
+          />
+        </div>
+        <div className="longStatsElem row d-flex justify-content-center">
+          <div className="col-md-8">
+            <ProgressBar variant="success" min={0} now={this.state.wordsNow} label={`${this.state.wordsNow}%`} />
+            <ProgressLabel />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
