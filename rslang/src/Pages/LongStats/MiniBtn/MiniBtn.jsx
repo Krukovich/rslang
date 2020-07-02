2642563d7b39365ab3b31a563e7e4561e9b2afa8
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-02 04:05:46 +0300

    mini games BTNs

--- rslang/src/Pages/LongStats/MiniBtn/MiniBtn.jsx
+++ rslang/src/Pages/LongStats/MiniBtn/MiniBtn.jsx
@@ -4,7 +4,11 @@ const MiniBtn = ({ label, showStats }) => {
   return (
     <button type='button'
     className='btn btn-outline-success'
+<<<<<<< HEAD
     onClick={showStats}>{label}
+=======
+    onClick={showStats}>Button {label}
+>>>>>>> mini games BTNs
     </button>
   )
 }
