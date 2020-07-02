25e1a8a02bd4b859dcf800bdabfa43d8bfe21645
Alexey <Shaht3r@gmail.com>
2020-07-03 00:29:26 +0300

    init: add speakit game files

--- rslang/src/Components/Router/Routes.js
+++ rslang/src/Components/Router/Routes.js
@@ -33,7 +33,7 @@ const mapActionToProps = {
   setDayLearningWords,
 }
 
-const sourceOpenRoutes = [
+const sourceRoutes = [
   {
     path: '/logout',
     component: LogOut,
---
+++ rslang/src/Pages/SpeakIt/Components/Modal/Modal.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Modal = () => {
+  return (
+    <div class="modal fade" id="result" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Statistics</h5>
+            <span class="ml-5" id="modalLevel"></span>
+            <span class="ml-5" id="modalDate"></span>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body" id="modal">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
