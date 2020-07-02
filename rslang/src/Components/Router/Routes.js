6a8683b1c0b9d2fd4fbb4976086b947cf758cbdc
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-02 04:05:46 +0300

    mini games BTNs

--- rslang/src/Components/Router/Routes.js
+++ rslang/src/Components/Router/Routes.js
@@ -88,14 +88,6 @@ const sourceCloseRoutes = [
   {
     path: '/long-stats',
     component: () => <LongStats totalWords={80} />,
-<<<<<<< HEAD
-    exact: true,
-  },
-  {
-    path: '/createanaccount',
-    component: CreateAccount,
-=======
->>>>>>> fix, refactor
     exact: true,
   },
   {
