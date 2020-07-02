f8fa23d2ee44bc566a2cdd372078ac80c688e96c
Alexey <Shaht3r@gmail.com>
2020-07-03 00:29:26 +0300

    init: add speakit game files

---
+++ rslang/src/Pages/SpeakIt/Components/Buttons/PlayGame.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayGame = () => {
+  return(
+    <button className="btn btn-info w-100" type="button">
+      Запись слова
+    </button>
+  );
+}
+
+export default PlayGame;
