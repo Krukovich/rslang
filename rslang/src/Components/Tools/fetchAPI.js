e511dba5f97bfd1524cb22c0e9d8d0075a007590
fizik003 <kassik0boks@gmail.com>
2020-07-05 19:39:48 +0300

    feat: add set/getSettings to fetchAPI

--- rslang/src/Components/Tools/fetchAPI.js
+++ rslang/src/Components/Tools/fetchAPI.js
@@ -1,5 +1,6 @@
 import * as Const from "../../constant";
 import { getCookie } from "../../Components/Tools/GetCoocke";
+import { connect } from "react-redux";
 
 export const fetchAPI = async (query, obj) => {
   console.log("fetchAPI start query" + query);
@@ -51,5 +52,35 @@ export const fetchAPI = async (query, obj) => {
     return content;
   }
 
-  //Add yours
+  if(query === 'setSettings'){
+    const rawResponse = await fetch(Const.API_LINK + `users/${getCookie('userId')}/settings`,{
+      method: 'PUT',
+      withCredentials: true,
+      headers: {
+        'Authorization': `Bearer ${getCookie('token')}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "optional": obj
+      })
+    })
+    const content = await rawResponse.json();
+    console.log(content)
+    return content;
+  }
+  if(query === 'getSettings'){
+    const rawResponse = await fetch(Const.API_LINK + `users/${getCookie('userId')}/settings`, {
+      method: 'GET',
+      withCredentials: true,
+      headers: {
+        'Authorization': `Bearer ${getCookie('token')}`,
+        'Accept': 'application/json',
+      }
+    });
+    const content = await rawResponse.json()
+    console.log(content.optional)
+    // return content;
+  }
+  // ADDYOUR
 };
