69ff5d8e32421db77c131a22f5cf8421d5846e13
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-07-06 19:30:10 +0300

    feat: add round change

--- rslang/src/Pages/FortuneGame/FortuneGame.jsx
+++ rslang/src/Pages/FortuneGame/FortuneGame.jsx
@@ -2,35 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 
-import SprintCard from '../../Components/SprintCard/SprintCard';
 import StartScreen from './StartScreen/StartScreen';
 import EndScreen from './EndScreen/EndScreen';
 
-import Yakub from './assets/wait.png'
-
 import './FortuneGame.scss';
+
+import right1 from './assets/AudioSamples/right/right1.mp3'
+import right2 from './assets/AudioSamples/right/right2.mp3'
+import right3 from './assets/AudioSamples/right/right3.mp3'
+import right4 from './assets/AudioSamples/right/right4.ogg'
+import right5 from './assets/AudioSamples/right/right5.mp3'
+import wrong1 from './assets/AudioSamples/wrong/wrong1.mp3'
+import wrong2 from './assets/AudioSamples/wrong/wrong2.mp3'
+import wrong3 from './assets/AudioSamples/wrong/wrong3.mp3'
+import wrong4 from './assets/AudioSamples/wrong/wrong4.mp3'
+import wrong5 from './assets/AudioSamples/wrong/wrong5.mp3'
+import wrong6 from './assets/AudioSamples/wrong/wrong6.mp3'
 
 class FortuneGame extends Component {
     constructor() {
         super();
         this.state = {
-            gameStarted: true,
+            speak: false,
+            gameStarted: false,
             gameEnded: false,
-            counter: 60,
             score: 0,
-            maxStreak: 0,
-            modifier: 1,
-            mixedArr: 0,
-            activeQuestion: 0,
             mistakeCount: 0,
             words: 0,
             alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            alphabetCheck: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
             cursed: [],
             engWordArr: [],
+            engWordArrCheck: [],
             rusWordArr: [],
             dialogue: 'Угадывайте!',
+            continue: false,
+            audio: 0,
         }
-
         this.engWordRef = React.createRef();
     }
 
@@ -59,20 +67,39 @@ class FortuneGame extends Component {
 
         this.setState({
             rusWordArr: wordObj.wordTranslate.toUpperCase().split(''),
-            engWordArr: wordObj.word.toUpperCase().split('')
+            engWordArr: wordObj.word.toUpperCase().split(''),
+            engWordArrCheck: wordObj.word.toUpperCase().split(''),
         });
+    }
 
-        // const engArr = wordObj.word.split('');
-        // const rusArr = wordObj.wordTranslate.split('');
+    alphabetCheckHandler = (event) => {
+        const index = this.state.alphabetCheck.indexOf(event.target.textContent);
+        let alph = this.state.alphabetCheck.split('');
+        alph[index] = 0;
+        alph = alph.join('');
 
-        // this.drawRusWord(rusArr);
-        // this.drawEngWord(engArr);
-        // this.drawWord(engArr, rusArr);
+        this.setState({
+            alphabetCheck: alph,
+        })
+    }
+
+    roundEndHandler = () => {
+        for (let char of this.state.engWordArrCheck) {
+            if (char !== 0) return
+        }
+
+        this.setState({
+            continue: true
+        })
     }
 
     letterClickHandler = (event) => {
         if (event.target.className.includes('Fortune-Letter_inactive')) return
-        event.target.classList.add('Fortune-Letter_inactive');
+
+        this.alphabetCheckHandler(event);
+
+        // event.target.classList.add('Fortune-Letter_inactive');
+
         if (this.state.engWordArr.includes(event.target.textContent)) {
             this.rightAnswerHandler(event);
         } else {
@@ -81,23 +108,82 @@ class FortuneGame extends Component {
     }
 
     rightAnswerHandler = props => {
-        const dialogueVariants = ['Абсолютно верно!', '', '', '', '', ''];
-        for (let i = 0; i < this.engWordRef.current.children.length; i += 1) {
-            if (props.target.textContent === this.engWordRef.current.children[i].textContent) {
-                this.engWordRef.current.children[i].classList.remove('Fortune-Cell_closed');
+        const word = this.state.engWordArrCheck;
+        const dialogueVariants = ['Абсолютно верно!',
+            'Есть такая буква!', 'Правильно!', 'Это гениально!', 'Вопрос для детского сада!'];
+        const audioVariants = [right1, right2, right3, right4, right5];
+
+        for (let i = 0; i < word.length; i += 1) {
+            if (props.target.textContent === word[i]) {
+                word[i] = 0;
+
+                this.setState((prevState) => {
+                    return {
+                        score: prevState.score + 100,
+                        engWordArrCheck: word
+                    }
+                });
             }
         }
-
         this.setState((prevState) => {
             return {
-                score: prevState.score + 100,
-                dialogue: 'Абсолютно верно!'
+                dialogue: dialogueVariants[Math.floor(Math.random() * Math.floor(dialogueVariants.length))],
+                speak: true,
             }
         });
+
+        const audio = new Audio(audioVariants[Math.floor(Math.random() * Math.floor(audioVariants.length))]);
+        audio.play();
+
+        setTimeout(() => {
+            this.setState({
+                speak: false,
+            })
+        }, 900)
+
+        this.roundEndHandler();
+    }
+
+    cellClassHandler = (filler = [], cellClass, checkArray) => {
+        let content = [<td></td>];
+        for (let i = 1; i < 15; i += 1) {
+
+            if (typeof filler[i - 1] === 'string') {
+                content.push(<td className={checkArray[i - 1] === 0 ? null : cellClass}>{filler[i - 1]}</td>)
+            } else {
+                content.push(<td>{filler[i - 1]}</td>)
+            }
+        }
+        return content
     }
 
     wrongAnswerHandler = props => {
-        alert('ass')
+        const dialogueVariants = ['Извините, ошибка!',
+            'Нет такой буквы!', 'Откройте букву... но в другом слове', 'Вы отвечаете не подумав!'];
+        const audioVariants = [wrong1, wrong2, wrong3, wrong4, wrong5, wrong6];
+
+        this.setState((prevState) => {
+            return {
+                dialogue: dialogueVariants[Math.floor(Math.random() * Math.floor(dialogueVariants.length))],
+                speak: true,
+                mistakeCount: prevState.mistakeCount + 1,
+            }
+        });
+
+        if (this.state.mistakeCount === 2) {
+            this.setState({
+                gameEnded: true,
+            })
+        }
+
+        const audio = new Audio(audioVariants[Math.floor(Math.random() * Math.floor(audioVariants.length))]);
+        audio.play();
+
+        setTimeout(() => {
+            this.setState({
+                speak: false,
+            })
+        }, 900)
     }
 
     drawCells = (filler = [], cellClass) => {
@@ -114,62 +200,89 @@ class FortuneGame extends Component {
         return content
     }
 
-    drawLetters = () => {
+    drawLetters = (checkArr) => {
         let content = [];
         const alphabet = this.state.alphabet.split('');
+        const alphCheck = checkArr.split('');
         alphabet.map((a, i) => {
-            content.push(<td onClick={this.letterClickHandler} className="Fortune-Letter" key={i}>{a}</td>)
+            content.push(<td
+                onClick={this.letterClickHandler}
+                className={alphabet[i] !== alphCheck[i] ? "Fortune-Letter Fortune-Letter_inactive" : "Fortune-Letter"}
+                key={'letter' + i}
+            >{alphabet[i]}</td>)
         })
         return content
     }
 
     start = props => {
-        // alert(this.state.words);
-        this.render();
+        try {
+            this.loadWord();
+
+            this.setState({
+                gameStarted: true,
+                dialogue: 'Угадывайте!',
+                alphabetCheck: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                continue: false,
+                mistakeCount: 0,
+                gameEnded: false,
+            })
+        } catch {
+            alert('Please wait for the words to load')
+        }
+    }
+
+    restart = () => {
         this.loadWord();
+
+        this.setState({
+            dialogue: 'Угадывайте!',
+            alphabetCheck: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            continue: false,
+            mistakeCount: 0,
+            gameEnded: false,
+            score: 0,
+        })
     }
 
     componentDidMount() {
-        // this.getWords();
-        // this.loadWords()
+        this.getWords();
+    }
+
+    addYakubClass() {
+        const buttonClass = ['Fortune-Yakub', 'mb-2', 'mt-2'];
+        if (this.state.speak) {
+            buttonClass.push('Fortune-Yakub_speak');
+        }
+        return buttonClass.join(' ')
     }
 
     render() {
-        this.getWords();
         if (!this.state.gameStarted) {
             return (
-                <div className="Fortune container mt-5">
-                </div >
+                <div className="Fortune container Fortune-Startscreen pt-5">
+                    <div className="row">
+                        <div className="md-col-12 w-100 p-3 d-flex flex-column justify-content-center align-items-center">
+                            <div className="d-flex align-items-center">
+                                <span className="mr-2">Сложность:</span>
+                                <select ref={this.selectRef} onClick={this.difficultyHandler} className="d-inline-block">
+                                    <option value="0">1</option>
+                                    <option value="1">2</option>
+                                    <option value="2">3</option>
+                                    <option value="3">4</option>
+                                    <option value="4">5</option>
+                                    <option value="5">6</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <StartScreen gameStart={this.start} />
+                </div>
             )
-            // return (
-            // <div className="Sprint container mt-5">
-            //     <div className="row">
-            //         <div className="md-col-12 w-100 p-3 d-flex flex-column justify-content-center align-items-center">
-            //             <div className="d-flex align-items-center">
-            //                 <span className="mr-2">Сложность:</span>
-            //                 <select ref={this.selectRef} onClick={this.difficultyHandler} className="d-inline-block">
-            //                     <option value="0">1</option>
-            //                     <option value="1">2</option>
-            //                     <option value="2">3</option>
-            //                     <option value="3">4</option>
-            //                     <option value="4">5</option>
-            //                     <option value="5">6</option>
-            //                 </select>
-            //             </div>
-            //         </div>
-            //     </div>
-            //     <StartScreen gameStart={this.gameStart} />
-            // </div>
-            // )
         } else if (this.state.gameEnded) {
-            return (
-                <div className="Fortune container mt-5">
-                </div >
-            )
-            // return <EndScreen
-            // score={this.state.score}
-            // restart={this.gameRestart}
-            // />
+            return <EndScreen
+                score={this.state.score}
+                restart={this.restart}
+            />
         } else {
             return (
                 <div className="Fortune container pt-5">
@@ -181,8 +294,9 @@ class FortuneGame extends Component {
                                 <div className="DialogueBar-Box">{this.state.dialogue}</div>
                                 <div className="DialogueBar-Triangle"></div>
                             </div>
-                            <div className="Fortune-Yakub mb-2 mt-2"></div>
-                            <Button onClick={this.start} className="Fortune-Btn_exit p-0">В музей поля чудес (на главную)</Button>
+                            <div className={this.addYakubClass()}></div>
+                            <Button onClick={() => { this.setState({ gameEnded: true }) }} className="Fortune-Btn_exit p-0 mb-1">В музей поля чудес (закончить)</Button>
+                            <Button onClick={this.start} variant="success" className={this.state.continue === true ? "Fortune-Btn_next p-1 Fortune-Btn_next_active" : "Fortune-Btn_next p-1"}>Следующее слово</Button>
                         </div>
                         <div className="col-md-8">
                             <div className="Fortune-Board w-100 h-100">
@@ -192,7 +306,7 @@ class FortuneGame extends Component {
                                             {this.drawCells()}
                                         </tr>
                                         <tr ref={this.engWordRef} className="Fortune-PlayRow">
-                                            {this.drawCells(this.state.engWordArr, 'Fortune-Cell_closed')}
+                                            {this.cellClassHandler(this.state.engWordArr, 'Fortune-Cell_closed', this.state.engWordArrCheck)}
                                         </tr>
                                         <tr>
                                             {this.drawCells()}
@@ -210,37 +324,17 @@ class FortuneGame extends Component {
                                 </table>
                             </div>
                             <div className="Fortune-Letters mt-2 d-flex">
-                                {this.drawLetters()}
-                                {/* <div className="Fortune-Letter">A</div>
-                                <div className="Fortune-Letter">B</div>
-                                <div className="Fortune-Letter">C</div>
-                                <div className="Fortune-Letter">D</div>
-                                <div className="Fortune-Letter">E</div>
-                                <div className="Fortune-Letter">F</div>
-                                <div className="Fortune-Letter">G</div>
-                                <div className="Fortune-Letter">H</div>
-                                <div className="Fortune-Letter">I</div>
-                                <div className="Fortune-Letter">J</div>
-                                <div className="Fortune-Letter">K</div>
-                                <div className="Fortune-Letter">L</div>
-                                <div className="Fortune-Letter">M</div>
-                                <div className="Fortune-Letter">N</div>
-                                <div className="Fortune-Letter">O</div>
-                                <div className="Fortune-Letter">P</div>
-                                <div className="Fortune-Letter">Q</div>
-                                <div className="Fortune-Letter">R</div>
-                                <div className="Fortune-Letter">S</div>
-                                <div className="Fortune-Letter">T</div>
-                                <div className="Fortune-Letter">U</div>
-                                <div className="Fortune-Letter">V</div>
-                                <div className="Fortune-Letter">W</div>
-                                <div className="Fortune-Letter">X</div>
-                                <div className="Fortune-Letter">Y</div>
-                                <div className="Fortune-Letter">Z</div> */}
+                                {this.drawLetters(this.state.alphabetCheck)}
                             </div>
                         </div>
-                        <div className="col-md-2 d-flex justify-content-center align-items-center">
+                        <div className="col-md-2 d-flex justify-content-center align-items-center flex-column">
                             <h3 className="Fortune-Score">{this.state.score}</h3>
+                            <div className="Fortune-Player mt-3"></div>
+                            <div className="Fortune-Progressbar w-100 d-flex justify-content-center align-items-center">
+                                <div className="progress w-100 ml-1 mr-1 mt-3">
+                                    <div className="progress-bar bg-danger" style={{ width: (100 - this.state.mistakeCount * 33) + '%' }} role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div >
--- rslang/src/Pages/FortuneGame/StartScreen/StartScreen.js
+++ rslang/src/Pages/FortuneGame/StartScreen/StartScreen.js
@@ -1,19 +1,21 @@
 import React from 'react';
-// import './EndScreen.scss';
+import './StartScreen.scss';
 import { Button } from 'react-bootstrap';
 
 const StartScreen = (props) => {
     return (
-        <div className="row">
-            <div className="md-col-12 p-5 d-flex flex-column justify-content-center align-items-center">
-                <h1 className="mb-4">Sprint</h1>
-                <p className="mb-4 text-center">How to play: you are given a word and a translation. If they match click 'Yes', if not - click 'No'. The streak of 4 right answers doubles your reward. 3 mistakes lead to game over.</p>
-                <p className="mb-4">Good Luck!</p>
+        <div className="row w-100 FortuneStartscreen">
+            <div className="md-col-12 w-100 p-3 d-flex flex-column justify-content-center align-items-center">
+                <div className="FortuneStartscreen-Logo"></div>
+                {/* <h1 className="mb-4">Поле чудес</h1> */}
+                <p className="mb-4 text-center mt-3">Как играть: дан перевод слова. Нужно составить слово на английском языке из алфавита в нижней части экрана.
+                3 ошибки в раунде ведут к поражению.</p>
+                {/* <p className="mb-4">Удачи!</p> */}
                 <Button onClick={props.gameStart} variant={'primary'}>
                     START
                 </Button>
             </div>
-        </div>
+        </div >
     )
 }
 
