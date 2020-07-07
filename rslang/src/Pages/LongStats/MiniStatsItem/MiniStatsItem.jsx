cf5084ca6fcfb08225b2b0547ba9d8a82fb56851
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-08 00:28:03 +0300

    mini stats test

---
+++ rslang/src/Pages/LongStats/MiniStatsItem/MiniStatsItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MiniStatsItem = ({ timestamp, newWords }) => {
+	const day = new Date(timestamp).toString().slice(4, 15);
+    return(
+        <li className="d-flex justify-content-around">
+					<span>Дата игры: {day}</span>
+					<span>Счет: {newWords}</span>
+        </li>
+    )
+}
+
+export default MiniStatsItem;
