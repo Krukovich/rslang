7406abfa097cd69e436ab95611065099d5a41c39
fizik003 <kassik0boks@gmail.com>
2020-07-07 03:13:33 +0300

    feat: add download settings in index.js

--- rslang/src/Components/Tools/fetchAPI.js
+++ rslang/src/Components/Tools/fetchAPI.js
@@ -19,11 +19,10 @@ export const fetchAPI = async (query, obj) => {
   }
   if (query === "words") {
     const rawResponse = await fetch(
-      Const.API_LINK + query + "?page=" + obj.page + "&group=" + obj.group 
+      Const.API_LINK + query + "?page=" + obj.page + "&group=" + obj.group
     );
     const content = await rawResponse.json();
     return content;
-    
   }
   if (query === "users-set-statistics") {
     const rawResponse = await fetch(
@@ -39,48 +38,59 @@ export const fetchAPI = async (query, obj) => {
         body: JSON.stringify({
           learnedWords: 999,
           optional: {
-            "minigames": [
+            minigames: [
               {
-                gamename: 'audiocall',
-                statistics: [obj]
-              }
-            ]        
-        }
-      })
-    })
+                gamename: "audiocall",
+                statistics: [obj],
+              },
+            ],
+          },
+        }),
+      }
+    );
     const content = await rawResponse.json();
     return content;
   }
 
-  if(query === 'setSettings'){
-    const rawResponse = await fetch(Const.API_LINK + `users/${getCookie('userId')}/settings`,{
-      method: 'PUT',
-      withCredentials: true,
-      headers: {
-        'Authorization': `Bearer ${getCookie('token')}`,
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "optional": obj
-      })
-    })
+  if (query === "setSettings") {
+    const rawResponse = await fetch(
+      Const.API_LINK + `users/${getCookie("userId")}/settings`,
+      {
+        method: "PUT",
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${getCookie("token")}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          optional: obj,
+        }),
+      }
+    );
     const content = await rawResponse.json();
-    console.log(content)
+    console.log(content);
     return content;
   }
-  if(query === 'getSettings'){
-    const rawResponse = await fetch(Const.API_LINK + `users/${getCookie('userId')}/settings`, {
-      method: 'GET',
-      withCredentials: true,
-      headers: {
-        'Authorization': `Bearer ${getCookie('token')}`,
-        'Accept': 'application/json',
-      }
-    });
-    const content = await rawResponse.json()
-    console.log(content.optional)
-    // return content;
+  if (query === "getSettings") {
+    try {
+      const rawResponse = await fetch(
+        Const.API_LINK + `users/${getCookie("userId")}/settings`,
+        {
+          method: "GET",
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${getCookie("token")}`,
+            Accept: "application/json",
+          },
+        }
+      );
+      const content = await rawResponse.json();
+      console.log(content.optional);
+      return content.optional;
+    } catch (err) {
+      console.log("Первый раз)");
+    }
   }
   // ADDYOUR
 };
