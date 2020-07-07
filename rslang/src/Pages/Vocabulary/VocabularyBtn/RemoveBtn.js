dc6b3fd5ce02403f4fab40b671175b78fc9a8766
Alexey <Shaht3r@gmail.com>
2020-07-08 01:28:22 +0300

    refactor: import function and code refactor

--- rslang/src/Pages/Vocabulary/VocabularyBtn/RemoveBtn.js
+++ rslang/src/Pages/Vocabulary/VocabularyBtn/RemoveBtn.js
@@ -1,11 +1,16 @@
 import React from 'react'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 const RemoveBtn = ({ index, remove }) => {
   return (
-    <button type="button" className="btn float-left" onClick={ () => remove(index) }>
-      <span><FontAwesomeIcon icon={ faTrashAlt } /></span>
+    <button
+      type="button"
+      className="btn float-left"
+      onClick={ () => remove(index) }
+    >
+      <FontAwesomeIcon icon={ faTrashAlt } />
     </button>
   )
 }
