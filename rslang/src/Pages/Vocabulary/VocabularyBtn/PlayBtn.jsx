2adb799a6eaf3660b9102cdfe17b15779bfe368d
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2020-07-07 17:02:09 +0300

    refactor: remove unnecessary function playExampleSound

--- rslang/src/Pages/Vocabulary/VocabularyBtn/PlayBtn.jsx
+++ rslang/src/Pages/Vocabulary/VocabularyBtn/PlayBtn.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+import { playExampleSound } from '../../../service'
 
 const PlayBtn = ({ src }) => {
-    const playExampleSound = (src) => {
-        const myAudio = new Audio();
-        myAudio.src = `https://raw.githubusercontent.com/Nickolay-Dudaryk/rslang-data/master/${ src }`;
-        myAudio.play();
-    }
-
     return (
         <button type="button" className="btn mr-2" onClick={ () => playExampleSound(src) }><span><FontAwesomeIcon icon={ faPlayCircle } /></span></button>
     )
