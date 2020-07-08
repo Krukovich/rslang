1a50f4f437464366dcb7ee9f4a7ccf65ba945e3d
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-08 23:12:02 +0300

    fix: key

--- rslang/src/Pages/LongStats/LongStats.jsx
+++ rslang/src/Pages/LongStats/LongStats.jsx
@@ -166,6 +166,14 @@ class LongStats extends React.Component {
     );
   }
 
+<<<<<<< HEAD
+=======
+  // componentWillUnmount() {
+  //   if(this._asyncRequest) {
+  //     this._asyncRequest.cancel();
+  //   }
+  // }
+>>>>>>> fix: key
   
   render() {    
     const { items } = this.state;  
