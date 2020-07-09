f0e0c84058f2261611d7ed38d2aa1a7a40dfa4ed
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2020-07-09 23:15:56 +0300

    fix: DifficultWords using redux data

--- rslang/src/Pages/Vocabulary/DifficultWords/DifficultWords.jsx
+++ rslang/src/Pages/Vocabulary/DifficultWords/DifficultWords.jsx
@@ -3,13 +3,26 @@ import { connect } from 'react-redux';
 
 const mapStateToProps = (store) => {
   return { 
-    difficultWords: store.appSettings.difficultWords,
+    difficultWords: store.playZone.difficultWords,
   }
 }
 
 const DifficultWords = (props) => {
   return(
-  <span> {props.difficultWords} </span>
+    <ul className="list-group mb-4">
+    {props.difficultWords.map((item) => {
+
+      return (
+        <li key={item.id} className="d-flex list-group-item justify-content-between align-items-center">
+
+          <div className="d-flex justify-content-start align-items-center">
+            <span className="mr-3 text-capitalize">{item.word}</span>
+          </div>
+
+        </li>
+      )
+    })}
+  </ul>
   )
 }
 
