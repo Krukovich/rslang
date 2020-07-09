2c584f748cfccc724c0d714d3cb051d4f2127dbb
Alexey <Shaht3r@gmail.com>
2020-07-09 23:41:44 +0300

    fix: fix bugs after merge

--- rslang/src/Components/Sidebar/Sidebar.js
+++ rslang/src/Components/Sidebar/Sidebar.js
@@ -10,18 +10,18 @@ export const Sidebar = (props) => {
     asideClass.class = 'aside flex-shrink-1 vh-100 p-5 bg-secondary position-fixed';
   }
 
-    return (
-        <aside className={asideClass.class}>
-            <ul className="nav nav-pills flex-column align-items-center">
-                <NavItem href='/' title='Главная' />
-                <NavItem href='/playzone' title='Изучение слов' />
-                <NavItem href='/long-stats' title='Статистика' />
-                <NavItem href='/learning-words' title='Словарь' />
-                <NavItem href='/minigames' title='Мини игры' />
-                <NavItem href='/promo' title='Промо' />
-                <NavItem href='/about' title='Команда' />
-                <NavItem href='/settings' title='Настройки' />
-            </ul>
-        </aside>
-    )
+  return (
+    <aside className={asideClass.class}>
+      <ul className="nav nav-pills flex-column align-items-center">
+        <NavItem href='/' title='Главная' />
+        <NavItem href='/play-zone' title='Изучение слов' />
+        <NavItem href='/long-stats' title='Статистика' />
+        <NavItem href='/learning-words' title='Словарь' />
+        <NavItem href='/minigames' title='Мини игры' />
+        <NavItem href='/promo' title='Промо' />
+        <NavItem href='/about' title='Команда' />
+        <NavItem href='/settings' title='Настройки' />
+      </ul>
+    </aside>
+  )
 }
