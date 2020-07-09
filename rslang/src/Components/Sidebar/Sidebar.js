8955a5b9f8380f18939568545cf8763e1dc50ac2
Alexey <Shaht3r@gmail.com>
2020-07-10 00:05:31 +0300

    fix: fix bugs after merge

--- rslang/src/Components/Sidebar/Sidebar.js
+++ rslang/src/Components/Sidebar/Sidebar.js
@@ -17,7 +17,7 @@ export const Sidebar = (props) => {
         <NavItem href='/play-zone' title='Изучение слов' />
         <NavItem href='/long-stats' title='Статистика' />
         <NavItem href='/learning-words' title='Словарь' />
-        <NavItem href='/minigames' title='Мини игры' />
+        <NavItem href='/mini-games' title='Мини игры' />
         <NavItem href='/promo' title='Промо' />
         <NavItem href='/about' title='Команда' />
         <NavItem href='/settings' title='Настройки' />
