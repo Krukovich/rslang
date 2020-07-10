8215924aeac99a97480c20c8d53a00b08859aae9
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-10 20:03:45 +0300

    hk savanna reducer

--- rslang/src/Pages/LongStats/MiniStatsItem/MiniStatsItem.jsx
+++ rslang/src/Pages/LongStats/MiniStatsItem/MiniStatsItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MiniStatsItem = ({ timestamp, newWords }) => {
 	const day = new Date(timestamp).toString().slice(4, 15);
     return(
-        <li className="d-flex justify-content-around">
+        <li className="d-flex justify-content-around miniStats-elem">
 					<span>Дата игры: {day}</span>
 					<span>Счет: {newWords}</span>
         </li>
