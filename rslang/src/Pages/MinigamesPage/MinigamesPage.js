52407e922376cb67339ccebc22e6e3c8fbb4dffa
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-10 18:12:33 +0300

    MiniGamesPage

--- rslang/src/Pages/MinigamesPage/MinigamesPage.js
+++ rslang/src/Pages/MinigamesPage/MinigamesPage.js
@@ -5,137 +5,138 @@ import './MiniGamesPage.scss';
 
 import pic1 from './assets/pic1.jpg';
 import FortuneLogo from './assets/logo-turbo.jpg';
+import SpeakItLogo from './assets/enjoy_small.png';
 
 class MiniGamesPage extends Component {
-    constructor() {
-        super();
-        this.state = {
-            sprintCard: {
-                title: 'Sprint',
-                description: '',
-                href: 'SprintGame',
-                class: 'Card-Image_training',
-                pic: pic1
-            },
-            audiocallCard: {
-                title: 'Audiocall',
-                description: '',
-                href: 'audiocall',
-                class: 'Card-Image_learning',
-                pic: pic1
-            },
-            fortuneCard: {
-                title: 'Turbo Wheel of Fortune',
-                description: '',
-                href: 'FortuneGame',
-                class: 'Card-Image_learning',
-                pic: FortuneLogo
-            },
-            savannahCard: {
-                title: 'Savannah',
-                description: '',
-                href: 'Savannah',
-                class: 'Card-Image_learning',
-                pic: pic1
-            },
-            englishpuzzleCard: {
-                title: 'English Puzzle',
-                description: '',
-                href: 'englishpuzzle',
-                class: 'Card-Image_learning',
-                pic: pic1
-            },
-            speakitCard: {
-                title: 'Speak It',
-                description: '',
-                href: 'speakit',
-                class: 'Card-Image_learning',
-                pic: pic1
-            },
-            clicked: false,
-        }
-        this.asideToggle = this.asideToggle.bind(this);
+  constructor() {
+    super();
+    this.state = {
+      sprintCard: {
+        title: 'Sprint',
+        description: '',
+        href: 'SprintGame',
+        class: 'Card-Image_training',
+        pic: pic1
+      },
+      audioCallCard: {
+        title: 'Audiocall',
+        description: '',
+        href: 'audiocall',
+        class: 'Card-Image_learning',
+        pic: pic1
+      },
+      fortuneCard: {
+        title: 'Turbo Wheel of Fortune',
+        description: '',
+        href: 'fortune-game',
+        class: 'Card-Image_learning',
+        pic: FortuneLogo
+      },
+      savannaCard: {
+        title: 'Savanna',
+        description: '',
+        href: 'Savanna',
+        class: 'Card-Image_learning',
+        pic: pic1
+      },
+      englishPuzzleCard: {
+        title: 'English Puzzle',
+        description: '',
+        href: 'englishpuzzle',
+        class: 'Card-Image_learning',
+        pic: pic1
+      },
+      speakItCard: {
+        title: 'Speak It',
+        description: '',
+        href: 'speakIt-start',
+        class: 'Card-Image_learning',
+        pic: SpeakItLogo,
+      },
+      clicked: false,
     }
+    this.asideToggle = this.asideToggle.bind(this);
+  }
 
-    asideToggle() {
-        this.setState({ clicked: !this.state.clicked });
-    }
+  asideToggle() {
+    this.setState({ clicked: !this.state.clicked });
+  }
 
-    render() {
-        const sprintCard = this.state.sprintCard;
-        const audiocallCard = this.state.audiocallCard;
-        const savannahCard = this.state.savannahCard;
-        const fortuneCard = this.state.fortuneCard;
-        const englishpuzzleCard = this.state.englishpuzzleCard;
-        const speakitCard = this.state.speakitCard;
+  render() {
+    const sprintCard = this.state.sprintCard;
+    const audioCallCard = this.state.audioCallCard;
+    const savannaCard = this.state.savannaCard;
+    const fortuneCard = this.state.fortuneCard;
+    const englishPuzzleCard = this.state.englishPuzzleCard;
+    const speakItCard = this.state.speakItCard;
 
-        return (
-            <div className="MinigamesPage row p-3">
-                <div className="col-md-12">
-                    <div className="Cards row pt-5">
-                        <div className="Cards-Wrapper col-md-6 h-100">
-                            <Card
-                                class={sprintCard.class}
-                                pic={sprintCard.pic}
-                                href={sprintCard.href}
-                                title={sprintCard.title}
-                                description={sprintCard.description}
-                            />
-                        </div>
-                        <div className="Cards-Wrapper col-md-6 h-100">
-                            <Card
-                                class={audiocallCard.class}
-                                pic={audiocallCard.pic}
-                                href={audiocallCard.href}
-                                title={audiocallCard.title}
-                                description={audiocallCard.description}
-                            />
-                        </div>
-                    </div>
-                    <div className="Cards row pt-2">
-                        <div className="Cards-Wrapper col-md-6 h-100">
-                            <Card
-                                class={savannahCard.class}
-                                pic={savannahCard.pic}
-                                href={savannahCard.href}
-                                title={savannahCard.title}
-                                description={savannahCard.description}
-                            />
-                        </div>
-                        <div className="Cards-Wrapper col-md-6 h-100">
-                            <Card
-                                class={englishpuzzleCard.class}
-                                pic={englishpuzzleCard.pic}
-                                href={englishpuzzleCard.href}
-                                title={englishpuzzleCard.title}
-                                description={englishpuzzleCard.description}
-                            />
-                        </div>
-                    </div>
-                    <div className="Cards row pt-2">
-                        <div className="Cards-Wrapper col-md-6 h-100">
-                            <Card
-                                class={speakitCard.class}
-                                pic={speakitCard.pic}
-                                href={speakitCard.href}
-                                title={speakitCard.title}
-                                description={speakitCard.description}
-                            />
-                        </div>
-                        <div className="Cards-Wrapper col-md-6 h-100">
-                            <Card
-                                class={fortuneCard.class}
-                                pic={fortuneCard.pic}
-                                href={fortuneCard.href}
-                                title={fortuneCard.title}
-                                description={fortuneCard.description}
-                            />
-                        </div>
-                    </div>
-                </div>
+    return (
+      <div className="MiniGamesPage row p-3">
+        <div className="col-md-12">
+          <div className="Cards row pt-5">
+            <div className="Cards-Wrapper col-md-6 h-100">
+              <Card
+                class={sprintCard.class}
+                pic={sprintCard.pic}
+                href={sprintCard.href}
+                title={sprintCard.title}
+                description={sprintCard.description}
+              />
             </div>
-        )
-    }
+            <div className="Cards-Wrapper col-md-6 h-100">
+              <Card
+                class={audioCallCard.class}
+                pic={audioCallCard.pic}
+                href={audioCallCard.href}
+                title={audioCallCard.title}
+                description={audioCallCard.description}
+              />
+            </div>
+          </div>
+          <div className="Cards row pt-2">
+            <div className="Cards-Wrapper col-md-6 h-100">
+              <Card
+                class={savannaCard.class}
+                pic={savannaCard.pic}
+                href={savannaCard.href}
+                title={savannaCard.title}
+                description={savannaCard.description}
+              />
+            </div>
+            <div className="Cards-Wrapper col-md-6 h-100">
+              <Card
+                class={englishPuzzleCard.class}
+                pic={englishPuzzleCard.pic}
+                href={englishPuzzleCard.href}
+                title={englishPuzzleCard.title}
+                description={englishPuzzleCard.description}
+              />
+            </div>
+          </div>
+          <div className="Cards row pt-2">
+            <div className="Cards-Wrapper col-md-6 h-100">
+              <Card
+                class={speakItCard.class}
+                pic={speakItCard.pic}
+                href={speakItCard.href}
+                title={speakItCard.title}
+                description={speakItCard.description}
+              />
+            </div>
+            <div className="Cards-Wrapper col-md-6 h-100">
+              <Card
+                class={fortuneCard.class}
+                pic={fortuneCard.pic}
+                href={fortuneCard.href}
+                title={fortuneCard.title}
+                description={fortuneCard.description}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default MiniGamesPage
