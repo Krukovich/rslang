7f5aa83af913ff259a89bf2699410340c5b1f058
Darya <45762789+DaryaYa@users.noreply.github.com>
2020-07-12 23:41:33 +0300

    console

--- rslang/src/Pages/LongStats/LongStats.jsx
+++ rslang/src/Pages/LongStats/LongStats.jsx
@@ -58,13 +58,9 @@ export default class LongStats extends React.Component {
     return [...arr.slice(0, idx), item, ...arr.slice(idx + 1)];
   };
 
-<<<<<<< HEAD
+
   showStats = (id) => {
     this.setState({ minigameSelect: this.state.items[id - 1].apiName }); //alexger
-=======
-  showStats = (id) => {  
-    this.setState({minigameSelect: this.state.items[id-1].apiName})//alexger
->>>>>>> long
     return this.state.items;
   };
 
@@ -77,30 +73,6 @@ export default class LongStats extends React.Component {
   }
 
   async componentDidMount() {
-<<<<<<< HEAD
-    let result = await fetchAPI("users-get-statistics").then(
-    );
-    this.setState({ count: result.optional });
-    let appStats = result.optional.appStats;
-    delete appStats[0];
-
-    const resultWords = Object.values(appStats);
-    const resultDate = Object.keys(appStats).map((item) => {
-      const data = Number(item);
-      const date = new Date(data).toString().slice(4, 15);
-      return date;
-    });
-    this.state.datasets[0].data = this.getSum(resultWords);
-    this.state.datasets[1].data = resultWords;
-    this.state.labels = resultDate;
-    this.state.wordsNow = Math.ceil(
-      (this.state.datasets[0].data[this.state.datasets[0].data.length - 1] *
-        100) /
-        this.props.totalWords
-    );
-    this._asyncRequest = null;
-    this.setState({ result });
-=======
   let result = await fetchAPI('users-get-statistics').then(console.log(('данные получены')));
       this.setState({count: result.optional})
       let appStats = result.optional.appStats;
@@ -119,7 +91,6 @@ export default class LongStats extends React.Component {
         this.setState({result});
       
     
->>>>>>> long
   }
 
   minigameSelect(selector) {
