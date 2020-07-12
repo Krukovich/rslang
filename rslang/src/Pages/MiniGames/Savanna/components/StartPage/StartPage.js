22717877d3355573d9d4ea97473dcb78a3918177
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-07-12 21:41:37 +0300

    feat: add longplay in savanna

--- rslang/src/Pages/MiniGames/Savanna/components/StartPage/StartPage.js
+++ rslang/src/Pages/MiniGames/Savanna/components/StartPage/StartPage.js
@@ -36,6 +36,11 @@ export class SavannaStartPage extends Component {
     this.state.quizQ = await createQuize(this.state.quizQ);
     console.log("start page", this.state.quizQ);
     this.setState({ gameStart: true });
+    // try {
+
+    // } catch {
+    //   alert('Подожди, пока не загрузятся слова!')
+    // }
   }
 
   optionSpawner = (amount, key) => {
