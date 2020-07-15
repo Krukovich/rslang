18f591abbab6ffbfc32f7768fbf6cff060f34095
Alexey <Shaht3r@gmail.com>
2020-07-15 23:24:12 +0300

    feat: add link to gethub

--- rslang/src/Components/Header/Header.js
+++ rslang/src/Components/Header/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+
+import { LogOutBtn } from "../../Components/Buttons/LogOut";
+import logo from '../../Pages/AboutPage/assets/git.png';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 import "./Header.scss";
-import { LogOutBtn } from "../../Components/Buttons/LogOut";
 
 const Header = (props) => {
   const buttonClass = ["btn", "btn_aside", "btn-primary"];
@@ -22,6 +26,9 @@ const Header = (props) => {
         </button>
       </div>
       <div className="Header-Right w-25 d-flex justify-content-end">
+        <a href="https://github.com/Krukovich/rslang">
+          <img className="Logo" src={ logo } alt="logo" />
+        </a>
         <LogOutBtn />
       </div>
     </nav>
