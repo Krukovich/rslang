d3d57134316d2876e08e5723d10abee2489c4600
Alexey <Shaht3r@gmail.com>
2020-07-15 11:04:48 +0300

    fix: fix size login form and position

--- rslang/src/Pages/Authentication/Login/LoginLayout.js
+++ rslang/src/Pages/Authentication/Login/LoginLayout.js
@@ -4,8 +4,8 @@ export class LoginLayout extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-5">
             <div className="Login">{this.props.children}</div>
           </div>
         </div>
